0a0ef4db84bcee2f1c108f39d523fc7f4b91528a
Sylwia Galka <sgalkabarnas@gmail.com>
2023-02-11 12:13:25 +0100

    ostatnia poprawka

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -1,20 +1,25 @@
 package pl.edu.agh.mwo.invoice;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 import pl.edu.agh.mwo.invoice.product.Product;
 
 public class Invoice {
-    private Collection<Product> products;
+   // private Collection<Product> products = new ArrayList<>();
+    private Map<Product, Integer> products = new HashMap<>();
 
     public void addProduct(Product product) {
-        // TODO: implement
-
+        products.put(product, 1)
     }
 
     public void addProduct(Product product, Integer quantity) {
-        // TODO: implement
+        for (int i=0; i < quantity; i++){
+            products.add(product);
+        }
     }
 
     public BigDecimal getSubtotal() {
@@ -28,10 +33,19 @@ public class Invoice {
     }
 
     public BigDecimal getTax() {
-        return null;
+        BigDecimal temp = new BigDecimal(0);
+        if (products == null)
+            return temp;
+        for (Product x: products){
+            temp = temp.add(x.getPriceWithTax().subtract(x.getPrice()));
+        }
+        return temp;
     }
 
     public BigDecimal getTotal() {
-        return null;
+        BigDecimal temp = new BigDecimal(0);
+        if (products == null)
+            return temp;
+        return getTax().add(getSubtotal());
     }
 }
